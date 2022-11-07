6dc01c4d074066290b15665511252fdc85540d76
Luka Vrabec <lvrabec.101@gmail.com>
2022-11-07 18:51:48 +0100

    first commit to master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import Alert from './components/Alert';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from './components/ExpenseList';
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
+import React from 'react';
+
+
+const initalExpenses = [
+
+  {id : uuidv4(), charge: "rent", amount:1600},
+  {id : uuidv4(), charge: "car payment", amount:400},
+  {id : uuidv4(), charge: "credit ard bill", amount:1200}
+]
+
+
+
 
 function App() {
+
+const [expenses,setExpenses]=useState(initalExpenses);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+<Alert />
+<h1>Budget calculator</h1>
+<main className='App'>
+<ExpenseForm/>
+<ExpenseList expenses={expenses} />
+</main>
+<h1>Total Spendind : <span className='total'>
+  ${expenses.reduce((acc,curr)=>{
+
+    return (acc+= curr.amount);
+  },0)}
+  </span>
+  </h1>
+</>      
+);
 }
 
 export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ExpenseItem = () => {
+  return (
+    <div>
+        hello from  ExpenseItem
+      
+    </div>
+  )
+}
+
+export default ExpenseItem
